400b7c1d6a32d1cd3d72cc6e3463d82b58563e43
aslan huang <lionhuang@163.com>
2013-02-21 23:37:18 +0800

    post

---
+++ models/post.js
@@ -0,0 +1,82 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: lionhuang
+ * Date: 13-2-21
+ * Time: 下午10:50
+ * To change this template use File | Settings | File Templates.
+ */
+var mongodb = require('./db');
+
+function Post(username, post, time){
+    this.user = username;
+    this.post = post;
+    if(time){
+        this.time = time;
+    }else{
+        this.time = new Date();
+    }
+}
+
+module.exports  = Post;
+
+Post.prototype.save = function save(callback){
+    // 存入mongodb的文档
+    var post = {
+        user : this.user,
+        post : this.post,
+        time : this.time
+    };
+
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);
+        }
+        // 读取 posts 集合
+        db.collection('post', function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            // 为 user 属性添加索引
+            collection.ensureIndex('user');
+            // 写入 posts 文档
+            collection.insert(post, {safe:true}, function(err, post){
+                mongodb.close();
+                callback(err, post);
+            })
+        })
+    })
+}
+
+Post.get = function get(username, callback){
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            var query = {};
+            if(username){
+                query.username = username;
+            }
+            collection
+                .find(query)
+                .sort({time:-1})
+                .toArray(function(err, docs){
+                    mongodb.close();
+                    if(err){
+                        callback(err, null);
+                    }
+                    var posts = [];
+                    docs.forEach(function(doc, index){
+                        var post = new Post(doc.user, doc.post, doc.time);
+                        posts.push(post);
+                    });
+                    callback(null, posts);
+                });
+        })
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 module.exports = function(app) {
     // 首页
@@ -115,4 +116,37 @@ module.exports = function(app) {
         }
         next();
     }
+
+    app.post('/post', function(req, res){
+        var currentUser = req.session.user;
+        var post = new Post(currentUser.name, req.body.post);
+        post.save(function(err){
+            if(err){
+                req.flash('error',err);
+                return res.redirect('/');
+            }
+            req.flash('success','成功');
+            res.redirect('/u/'+currentUser.name);
+        })
+    });
+
+    app.get('/u/:user', function(req,res){
+        User.get(req.params.user, function(err, user){
+            if(!user){
+                req.flash('error','用户不存在');
+                return res.redirect('/');
+            }
+            Post.get(user.name, function(err, posts){
+                if(err){
+                    req.flash('error',err);
+                    return   res.redirect('/');
+                }
+                res.render('user',{
+                    title: user.name,
+                    posts: posts,
+                    user : req.params.user
+                });
+            });
+        })
+    })
 }
